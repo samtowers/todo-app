6f4206c907463ef40f07cab5d11f9a15cebb10d8
Sam <jto.sam@gmail.com>
2022-05-14 21:03:39 +0100

    Render checkboxes + fix scenario w/ zero tasks

--- src/components/TaskDashboard.tsx
+++ src/components/TaskDashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import Tabs, {Tab} from './Tabs'
 import Checkboxes, {Checkbox} from "./Checkboxes";
 import TaskAdd from "./TaskAdd";
@@ -28,15 +28,27 @@ export default class TaskDashboard extends React.Component<any, TaskDashboardSta
     }
 
     addItem(item: string) {
-        this.setState({tasks: this.state.tasks.addItem(item)});
+        this.setState({tasks: this.state.tasks.add(item)});
     }
 
-    toggleItem() {
-
+    updateItem(id: number, done: boolean) {
+        this.setState({tasks: this.state.tasks.update(id, done)});
     }
 
     deleteCompletedTasks() {
         console.log('deleteCompletedTasks');
+    }
+
+    /**
+     * @param filterDone Set to apply filter. Omit to NOT filter at all on `done` property.
+     */
+    *renderCheckboxes(filterDone?: boolean): Generator<ReactElement> {
+        for (const [id, task] of Object.entries(this.state.tasks.items)) {
+            if (filterDone !== undefined && task.done !== filterDone) {
+                continue;
+            }
+            yield <Checkbox key={id} name={task.name} checked={task.done}/>;
+        }
     }
 
     render() {
@@ -64,28 +76,18 @@ export default class TaskDashboard extends React.Component<any, TaskDashboardSta
                             //     `key` can be set on container <Checkboxes> or on each individual <checkbox>.
                             // https://stackoverflow.com/questions/39549424/how-to-create-unique-keys-for-react-elements
                         }
-                        <Checkboxes key="all">
-                            {/* fixme: DRY */}
-                            {this.state.tasks.current.map((task, idx) => {
-                                return <Checkbox name={task} checked={false}/>
-                            })}
-                            {this.state.tasks.done.map((task: string) => {
-                                return <Checkbox name={task} checked={true}/>
-                            })}
+                        <Checkboxes>
+                            {Array.from(this.renderCheckboxes())}
                         </Checkboxes>
                     </Tab>
                     <Tab name="Active" onClick={onTabClick}>
-                        <Checkboxes key="active">
-                            {this.state.tasks.current.map((task: string) => {
-                                return <Checkbox name={task} checked={false}/>
-                            })}
+                        <Checkboxes>
+                            {Array.from(this.renderCheckboxes(false))}
                         </Checkboxes>
                     </Tab>
                     <Tab name="Completed" onClick={onTabClick}>
-                        <Checkboxes key="done">
-                            {this.state.tasks.done.map((task: string) => {
-                                return <Checkbox name={task} checked={true}/>
-                            })}
+                        <Checkboxes>
+                            {Array.from(this.renderCheckboxes(true))}
                         </Checkboxes>
                         <div className="flex justify-end">
                             <button
@@ -128,7 +130,7 @@ interface Task {
 // }
 
 /**
- * Immutable list of tasks completed.
+ * Immutable list of task item.
  */
 class TaskList {
     // Explicit incrementing ID. Used for react keys.
@@ -138,30 +140,33 @@ class TaskList {
      * @param items
      */
     // st: https://stackoverflow.com/questions/36467469/is-key-value-pair-available-in-typescript
-    constructor(items?: {[id: number]: Task }) {
+    constructor(items?: { [id: number]: Task }) {
         this.items = items || {};
     }
 
-    getLastId(): number {
-        // `Object.keys`: object key order is well-defined for integers keys in ES2020. Ascending order is expected.
-        // See: https://stackoverflow.com/questions/30076219/
-        const keys = Object.keys(this.items);
-        return Number(keys[keys.length - 1]);
+    // fixme: Remove unused:
+    public* filterItems(done: boolean): Generator<[id: number, task: Task]> {
+        for (const [id, task] of Object.entries(this.items)) {
+            if (task.done === done) {
+                yield [Number(id), task];
+            }
+        }
     }
 
-    addItem(name: string, done: boolean = false): TaskList {
-        const newId = this.getLastId() + 1;
-        return new TaskList(Object.assign(this.items, {newId: {name, done}}))
+    public add(name: string, done: boolean = false): TaskList {
+        let newItems = {...this.items};
+        newItems[this.getLastId() + 1] = {name, done};
+        return new TaskList(newItems);
     }
 
     // fixme: Use of shallow copies?
-    removeItem(id: number): TaskList {
+    public remove(id: number): TaskList {
         let newItems = {...this.items}; // Shallow clone items.
         delete newItems[id];
         return new TaskList(newItems);
     }
 
-    updateItem(id: number, done: boolean): TaskList {
+    update(id: number, done: boolean): TaskList {
         let newItems = {...this.items};
         // Update task:
         // st: NB: First arg in `Object.assign` will NOT be cloned, but updated. Which is bad for immutability.
@@ -178,5 +183,15 @@ class TaskList {
             }
         }
         return new TaskList(newItems);
+    }
+
+    private getLastId(): number {
+        // `Object.keys`: object key order is well-defined for integers keys in ES2020. Ascending order is expected.
+        // See: https://stackoverflow.com/questions/30076219/
+        const keys = Object.keys(this.items);
+        if (keys.length === 0) {
+            return 0;
+        }
+        return Number(keys[keys.length - 1]);
     }
 }
