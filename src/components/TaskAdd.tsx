9f0a69f2ec2f9af23a84d8af84eae19822204fcc
Sam <jto.sam@gmail.com>
2022-05-27 16:35:01 +0100

    Refactor TaskAdd

--- src/components/TaskAdd.tsx
+++ src/components/TaskAdd.tsx
@@ -2,46 +2,44 @@ import React from "react";
 
 // fixme: Keyboard events. Enter & optionally Esc.
 
+interface ClassAddProps {
+    onItemAdd: Function,
+}
 
 /**
  * Text input & button to add to-do items.
  */
-export default class TaskAdd extends React.Component<{ onItemAdd: Function }> {
+export default function TaskAdd({onItemAdd}: ClassAddProps) {
+    return (
+        <div className="flex flex-wrap mb-3">
+            <input
+                className="py-2 px-3 flex-1 mr-2 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                onKeyUp={ev => {
+                    if (ev.key === 'Enter') {
+                        processInput(onItemAdd, ev.target as HTMLInputElement)
+                    }
+                }}
+                placeholder="E.g. Grab pint of milk"
+            />
 
-    private onInputKeyUp(ev: React.KeyboardEvent<HTMLInputElement>) {
-        if (ev.key !== 'Enter') {
-            return;
-        }
-        this.processInput(ev.target as HTMLInputElement)
+            <button
+                className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400 cursor-pointer"
+                onClick={ev => {
+                    let input = (ev.target as Element).previousElementSibling as HTMLInputElement;
+                    processInput(onItemAdd, input);
+                }}
+            >
+                Add
+            </button>
+        </div>
+    );
+}
+
+function processInput(onItemAdd: Function, input: HTMLInputElement) {
+    if (!input.value) {
+        return;
     }
-
-    private processInput(input: HTMLInputElement) {
-        if (!input.value) {
-            return;
-        }
-        this.props.onItemAdd(input.value);
-        input.value = '';
-    }
-
-    render() {
-        return (
-            <div className="flex flex-wrap mb-3">
-                {/* st: Clear option only available on type=search. Do not include this. */}
-                <input
-                    className="py-2 px-3 flex-1 mr-2 shadow appearance-none border rounded text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
-                    onKeyUp={ev => this.onInputKeyUp(ev)}
-                    placeholder="E.g. Grab pint of milk"
-                />
-
-                <button
-                    className="py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-400 cursor-pointer"
-                    // fixme: More elegant approach?
-                    onClick={ev => this.processInput((ev.target as HTMLButtonElement).previousElementSibling as HTMLInputElement)}
-                >
-                    Add
-                </button>
-            </div>
-        );
-    }
+    onItemAdd(input.value);
+    input.value = '';
 }
